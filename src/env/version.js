4e2c5f7eb8c9413c85e2a630c3a6d819f8b703ce
Tyago Veras <tyagoveras@gmail.com>
2025-06-25 09:19:24 -0300

    resolve merge conflit

--- src/env/version.js
+++ src/env/version.js
@@ -1,1 +1,1 @@
-module.exports = { VERSION: '0.14.13' }
+module.exports = { VERSION: '0.2.0' }
--- src/schemas/index.js
+++ src/schemas/index.js
@@ -3,19 +3,13 @@
 const DistribuicaoSchema = require('./distribuicaoDFe-schema')
 const RecepcaoSchema = require('./recepcaoEvento-schema')
 const DistribuicaoCteSchema = require('./distribuicaoCte-schema')
-<<<<<<< HEAD
-=======
 const DistribuicaoMdfeSchema = require('./distribuicaoMdfe-schema')
->>>>>>> v80pxt-codex/adapt-project-to-search-cte-using-certificate-a1
 
 const schema = Object.freeze({
   DistribuicaoSchema,
   RecepcaoSchema,
   DistribuicaoCteSchema,
-<<<<<<< HEAD
-=======
   DistribuicaoMdfeSchema,
->>>>>>> v80pxt-codex/adapt-project-to-search-cte-using-certificate-a1
 })
 
 module.exports = schema
